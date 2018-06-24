b96ecf6ff539479976bfbcbac23f8b145d7ebf85
xCeLeSx <celes15@o2.pl>
2018-06-24 10:04:24 +0200

    pages

--- js/temp.js
+++
@@ -1,9 +0,0 @@
-$("#box-temp").click(function() {
-    $("#temp").load("temp.php");
-    var x = document.getElementById("snackbar");
- });
-
-setInterval(function () {
-  $("#temp").load("temp.php");
-}, 10000);
-
--- temp.php
+++
@@ -1,4 +0,0 @@
-<?php
-$output = shell_exec("python temp.py c");
-echo $output;
-?>
